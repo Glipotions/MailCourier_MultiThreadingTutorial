2d979295beda221b614dff8df0523a555399dfed
Glipotions <hamzakavak1905@gmail.com>
2022-09-10 22:22:54 +0300

    Proje dosyası ekle.

---
+++ MultiThreading/MailProviders/GoogleMailProvider.cs
@@ -0,0 +1,17 @@
+﻿using MultiThreading.MailProviders.Base;
+using MultiThreading.Models;
+using System.Threading.Tasks;
+
+namespace MultiThreading.MailProviders
+{
+    public class GoogleMailProvider : BaseProvider
+    {
+        public override async Task Send(MailObject mailObject)
+        {
+            // Send email
+
+            await Task.Delay(3000);
+            LogManager.Default.Log($"GoogleMail has sent to {mailObject.To}");
+        }
+    }
+}
